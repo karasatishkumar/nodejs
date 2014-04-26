a62c7d40b06ee529ee97c5e7f6a9e0ff44c70de1
karasatishkumar <karasatishkumar@gmail.com>
2014-04-27 00:36:03 +0530

    introduction of the request hadler

--- server.js
+++ server.js
@@ -4,13 +4,13 @@
 var http = require("http");
 var url = require("url");
 
-function start(route){
+function start(route, handle){
     function onRequest(request, response) {
                 
         var pathName = url.parse(request.url).pathname;
         console.log("Request for " +pathName + " received.");
         
-        route(pathName);
+        route(handle, pathName);
         
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello World");
