c2043261caf5d19a92503a34a4947a83415662fb
karasatishkumar <karasatishkumar@gmail.com>
2014-04-27 00:13:11 +0530

    router introduction

--- server.js
+++ server.js
@@ -4,11 +4,13 @@
 var http = require("http");
 var url = require("url");
 
-function start(){
+function start(route){
     function onRequest(request, response) {
                 
         var pathName = url.parse(request.url).pathname;
         console.log("Request for " +pathName + " received.");
+        
+        route(pathName);
         
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello World");
