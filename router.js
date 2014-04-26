c96f5218753777c9370171a80e4a595e2210981c
karasatishkumar <karasatishkumar@gmail.com>
2014-04-27 02:02:21 +0530

    provision for the post request

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
     console.log("About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
-        return handle[pathname].call(this, response);
+        return handle[pathname].call(this, response, postData);
     } else {
         console.log("No request handler found for " + pathname);
         return "404 Not Found";
--- server.js
+++ server.js
@@ -6,11 +6,22 @@ var url = require("url");
 
 function start(route, handle){
     function onRequest(request, response) {
-                
+        var postData = "";        
         var pathName = url.parse(request.url).pathname;
         console.log("Request for " +pathName + " received.");
         
-        var content = route(handle, pathName, response);
+        request.setEncoding("utf8");
+        
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '"+
+            postDataChunk + "'.");
+        });
+        
+        request.addListener("end", function() {
+            route(handle, pathName, response, postData);
+        });
+        
         
     }
     
