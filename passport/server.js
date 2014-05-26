f91f8850c8db698e9012b9b4898420f190379480
karasatishkumar <karasatishkumar@gmail.com>
2014-05-27 00:14:30 +0530

    login and signup functionality

--- passport/server.js
+++ passport/server.js
@@ -14,7 +14,7 @@ var configDB = require('./config/database.js');
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 
-// require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 app.configure(function() {
 
